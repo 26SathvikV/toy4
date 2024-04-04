1b2752db93bf18fdee307cb802e247b4aec653aa
26SathvikV <156643595+26SathvikV@users.noreply.github.com>
2024-04-04 10:03:38 -0700

    Full Game
    Win Condition isn't working

---
+++ project/resources/levels/level1.gd
@@ -0,0 +1,22 @@
+extends GridMap
+
+var START_POINT: Node3D;
+var END_POINT: Area3D;
+var CLONE_TRIGGERS: Array[Area3D] = [];
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	START_POINT = $start;
+	END_POINT = $end;
+	
+	for child in self.get_children():
+		if (child.is_in_group("clone_trigger")):
+			CLONE_TRIGGERS.append(child);
+
+func notifyDeath():
+	if (get_tree().get_nodes_in_group("players").size() == 0):
+		$GUI.changeGUI(2);
+
+
+func _on_end_area_entered(area):
+	$GUI.changeGUI(3);
